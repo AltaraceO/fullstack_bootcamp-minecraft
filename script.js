434a50900d659a2335e2dad0dc6a77b909128071
AltaraceO <orialtarace@gmail.com>
2021-12-03 23:05:27 +0200

    remove event listener

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const axe = document.querySelector(".axe");
 const storage = document.querySelector(".storage");
 const startBtn = document.querySelector(".start-button");
 const gameStart = document.querySelector(".game-start");
+const threeBtns = document.querySelector(".three-buttons");
 
 const colors = {
   green: "green",
@@ -82,31 +83,76 @@ function minecraftMain() {
     gameStart.style.display = "none";
     gameBoard.style.display = "grid";
   });
-  gameBoard.addEventListener("click", gameBoardtoStorage);
+  // gameBoard.addEventListener("click", gameBoardtoStorage);
 }
 minecraftMain();
 
 // function toolSelect() {}
 
-function gameBoardtoStorage(e) {
-  if (
-    e.target.className !== "blue" &&
-    e.target.className !== "white" &&
-    e.target.className !== "game-board"
-  ) {
-    const targetCls = e.target.getAttribute("class");
-    const currentStoreCls = storage.className;
+threeBtns.addEventListener("click", checkTool);
 
-    storage.classList.replace(currentStoreCls, targetCls);
-    storage.style.width = "30px";
-    storage.style.height = "30px";
+let currentEvent;
 
-    e.target.setAttribute("class", "blue");
+function checkTool(e) {
+  // console.log(e.target.className);
+  switch (e.target.className) {
+    case "pickaxe tool":
+      gameEvnListener(stoneFunc);
+
+      break;
+    case "shovel tool":
+      gameEvnListener(dirtFunc);
+      // gameBoard.removeEventListener("click", stoneFunc);
+      break;
+    case "axe tool":
+      gameEvnListener(treeFunc);
+      break;
+
+    default:
+      break;
   }
 }
 
+function gameEvnListener(material) {
+  gameBoard.addEventListener("click", material);
+  // currentEvent = material;
+  // console.log(currentEvent);
+  // console.log(typeof currentEvent);
+}
+
+function stoneFunc(e) {
+  if (e.target.className === "gray") {
+    pickFunc(e);
+  }
+}
+
+function dirtFunc(e) {
+  // gameBoard.removeEventListener("click", stoneFunc);
+  if (e.target.className === "brown") {
+    pickFunc(e);
+  }
+}
+
+function treeFunc(e) {
+  if (e.target.className === "darkbrown" || e.target.className === "green") {
+    pickFunc(e);
+  }
+}
+
+function pickFunc(e) {
+  const targetCls = e.target.getAttribute("class");
+  const currentStoreCls = storage.className;
+
+  storage.classList.replace(currentStoreCls, targetCls);
+  storage.style.width = "30px";
+  storage.style.height = "30px";
+
+  e.target.setAttribute("class", "blue");
+  this.removeEventListener("click", arguments.callee);
+}
+
 storage.addEventListener("click", function (e) {
-  gameBoard.removeEventListener("click", gameBoardtoStorage);
+  // gameBoard.removeEventListener("click", material);
   gameBoard.addEventListener("click", storageToBox);
 });
 
@@ -116,5 +162,5 @@ function storageToBox(e) {
     storage.setAttribute("class", "storage");
   }
   this.removeEventListener("click", arguments.callee);
-  gameBoard.addEventListener("click", gameBoardtoStorage);
+  // gameBoard.addEventListener("click", gameBoardtoStorage);
 }
